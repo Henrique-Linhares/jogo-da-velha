9ca0064d4f9ee398ecf3ad80a0f0437eab50bacc
Henrique-Linhares <hlinhares453@gmail.com>
2025-09-01 22:37:42 -0300

    Correção do erro

--- src/App.js
+++ src/App.js
@@ -101,10 +101,11 @@ function Tabuleiro({ xIsNext, squares, onPlay }) {
       </div>
     </div>
   );
-}
+}; 
+
 //Componente Game
 export default function Game() {
-  const [history, setHistory] = useState([Array[9].fill(null)]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
